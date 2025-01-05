4456806aec258c4585db2d97336a80945871ab8e
Supitpong Wongsaroj <ewrctysf@students.codeinstitute.net>
2025-01-05 13:27:48 +0000

    Add code for reduce challenge

--- reduce.js
+++ reduce.js
@@ -72,3 +72,43 @@ let nameProfession = teamMembers.reduce((acc, curr) => {
   return acc;
 }, {})
 console.log(nameProfession);
+
+let students = [
+  {
+      name: 'John',
+      subjects: ['maths', 'english', 'cad'],
+      teacher: {maths: 'Harry', english: 'Joan', cad: 'Paul'},
+      results: {maths: 90, english: 75, cad: 87},
+  },
+  {
+      name: 'Emily',
+      subjects: ['science', 'english', 'art'],
+      teacher: {science: 'Iris', english: 'Joan', art: 'Simon'},
+      results: {science: 93, english: 73, art: 95},
+  },
+  {
+      name: 'Adam',
+      subjects: ['science', 'maths', 'art'],
+      teacher: {science: 'Iris', maths: 'Harry', art: 'Simon'},
+      results: {science: 93, english: 88, maths: 97, art: 95},
+  },
+  {
+      name: 'Fran',
+      subjects: ['science', 'english', 'art'],
+      teacher: {science: 'Iris', english: 'Joan', art: 'Simon'},
+      results: {science: 93, english: 87, art: 95},
+  }
+];
+
+// expected outcome { name: 'Adam', max: 88 } initial value = { name: "", max: 0 }
+
+const biggest = students.reduce((acc, curr) => {
+  let englishScore = curr.results.english;
+  console.log(englishScore);
+  acc = acc.max > englishScore ? acc: {name: curr.name, max: englishScore}
+  return acc;
+}, { name: "", max: 0 });
+
+console.log(biggest);
+
+
